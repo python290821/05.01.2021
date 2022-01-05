0d62e261afe68b86dfcd840128976db20134b913
micserv010821 <dotnet290320@gmail.com>
2022-01-05 20:30:50 +0200

    upload

---
+++ calc_test.py
@@ -0,0 +1,8 @@
+
+# 1 test_total_empty
+# 2 test_total_single_item 110.0
+# 3 test_total_multiple_items 1.0, 2.0, 3.0
+# 4 test_total_various_inputs (xs, expected) parameterized
+#    ([], 0.0)
+#    ([110.0], 110)
+#    ([1.0, 2.0, 3.0], 6)
