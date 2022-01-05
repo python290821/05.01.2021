5beb3366f543506882cd9bba37f60cdbb669281d
micserv010821 <dotnet290320@gmail.com>
2022-01-05 22:05:53 +0200

    upload

---
+++ dao.py
@@ -0,0 +1,9 @@
+import time
+class Dao:
+    def __init__(self):
+        #time.sleep(3)
+        pass
+    def get_all(self):
+        return [1,2,3,4,5]
+    def get_first(self) -> list[int]:
+        return [1]
---
+++ dao_test.py
@@ -0,0 +1,24 @@
+import pytest
+from dao import Dao
+import time
+
+@pytest.fixture(scope='session', autouse=True)
+def dao_init():
+    time.sleep(3)
+    print('setting up stuff')
+    yield()
+    print('cleanup')
+    time.sleep(3)
+
+@pytest.fixture(scope='session')
+def dao_connection():
+    print('setting up dao')
+    return Dao()
+
+def test_get_all(dao_connection):
+    dao_connection.a = 1
+    assert dao_connection.get_all() == [1, 2, 3, 4, 5]
+
+def test_get_first(dao_connection):
+    assert dao_connection.get_first() == [dao_connection.a]
+
