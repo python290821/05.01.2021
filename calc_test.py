86959996e0651c200902c71b695863624e9a8186
micserv010821 <dotnet290320@gmail.com>
2022-01-05 21:11:47 +0200

    Update calc_test.py

--- calc_test.py
+++ calc_test.py
@@ -1,8 +1,19 @@
+from calc import total
+import typing
+import pytest
 
-# 1 test_total_empty
-# 2 test_total_single_item 110.0
-# 3 test_total_multiple_items 1.0, 2.0, 3.0
-# 4 test_total_various_inputs (xs, expected) parameterized
-#    ([], 0.0)
-#    ([110.0], 110)
-#    ([1.0, 2.0, 3.0], 6)
+def test_total_empty() -> None:
+    assert total([]) == 0
+
+def test_total_empty() -> None:
+    assert total([110.0]) == 110.0
+
+def test_total_empty() -> None:
+    assert total([1.0, 2.0, 3.0]) == 6.0
+
+@pytest.mark.parametrize(('xs', 'expected'),(
+        ([], 0.0),
+        ([110.0], 110),
+        ([1.0, 2.0, 3.0], 6.0)))
+def test_total_various_inputs(xs, expected):
+    assert total(xs) == expected
